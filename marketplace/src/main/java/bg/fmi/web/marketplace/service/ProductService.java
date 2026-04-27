2c6acdca34fa46fcd5c2880d5d6d44341bba6118
Nikolay Milkov <nikolay.milkov04@gmail.com>
2026-04-27 23:19:00 +0300

    added product service entity

---
+++ marketplace/src/main/java/bg/fmi/web/marketplace/service/ProductService.java
@@ -0,0 +1,17 @@
+package bg.fmi.web.marketplace.service;
+
+import bg.fmi.web.marketplace.model.product.Product;
+import bg.fmi.web.marketplace.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public Long addProduct(Product product) {
+        return productRepository.save(product).getId();
+    }
+}
