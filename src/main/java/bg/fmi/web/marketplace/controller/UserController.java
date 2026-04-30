ed748890278ec38fa6272887d109b509b87a4fc3
Stoyan Yordanov <137334856+forklift-operator@users.noreply.github.com>
2026-04-30 22:08:32 +0300

    Added getUser (by id) method

--- src/main/java/bg/fmi/web/marketplace/controller/UserController.java
+++ src/main/java/bg/fmi/web/marketplace/controller/UserController.java
@@ -5,10 +5,12 @@ import bg.fmi.web.marketplace.dto.UserLoginDto;
 import bg.fmi.web.marketplace.dto.UserResponseDto;
 import bg.fmi.web.marketplace.model.user.User;
 import bg.fmi.web.marketplace.service.UserService;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,6 +42,20 @@ public class UserController {
 
                     return userResponse;
                 }).toList();
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<UserResponseDto> getUser(@PathVariable @NotNull Long id) {
+        User user = userService.getUserById(id);
+
+        UserResponseDto userResponse = new UserResponseDto();
+        userResponse.setId(user.getId());
+        userResponse.setFirstName(user.getFirstName());
+        userResponse.setLastName(userResponse.getLastName());
+        userResponse.setEmail(user.getEmail());
+        userResponse.setRole(user.getRole());
+
+        return ResponseEntity.status(HttpStatus.OK).body(userResponse);
     }
 
     @PostMapping("/register")
