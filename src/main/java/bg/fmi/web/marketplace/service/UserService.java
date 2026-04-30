b0b70358797e8e8c4a5b6d889b2c4e5e8942cbab
Stoyan Yordanov <137334856+forklift-operator@users.noreply.github.com>
2026-04-30 22:08:04 +0300

    Refactored the user service and repo to use Optional and added findById method

--- src/main/java/bg/fmi/web/marketplace/service/UserService.java
+++ src/main/java/bg/fmi/web/marketplace/service/UserService.java
@@ -3,7 +3,6 @@ package bg.fmi.web.marketplace.service;
 import bg.fmi.web.marketplace.model.user.User;
 import bg.fmi.web.marketplace.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,29 +13,46 @@ public class UserService {
 
     @Autowired
     public UserService(UserRepository userRepository) {
+
         this.userRepository = userRepository;
+
     }
 
     public User register(User user) {
-        User foundUser = userRepository.findByEmail(user.getEmail());
-        if (foundUser != null) throw new RuntimeException("User with this email already exists");
+
+        userRepository.findByEmail(user.getEmail())
+                .ifPresent(existingUser -> {
+                    throw new RuntimeException("User with this email already exists");
+                });
+
 //        user.setPassword(encoder.encode(user.getPassword()));
+
         return userRepository.save(user);
+
     }
 
     public List<User> getAllUsers() {
+
         return userRepository.findAll();
+
     }
 
     public User login(User userRequest) {
-        User foundUser = userRepository.findByEmail(userRequest.getEmail());
 
-        if (foundUser == null) throw new RuntimeException("User with this email not found");
+        User foundUser = userRepository.findByEmail(userRequest.getEmail())
+                .orElseThrow(() -> new RuntimeException("Invalid email"));
 
         if (foundUser.getPassword().equals(userRequest.getPassword())) {
             return foundUser;
         } else {
             throw new RuntimeException("Incorrect password");
         }
+
+    }
+
+    public User getUserById(Long id) {
+
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User with this id not found"));
     }
 }
