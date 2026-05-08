bc420a76dce15214a916a2f8662e4571a818f1c2
Stoyan Yordanov <137334856+forklift-operator@users.noreply.github.com>
2026-05-08 12:52:03 +0300

    Added invalid state or resource

---
+++ src/main/java/bg/fmi/web/marketplace/exception/InvalidStateOfResourceException.java
@@ -0,0 +1,7 @@
+package bg.fmi.web.marketplace.exception;
+
+public class InvalidStateOfResourceException extends RuntimeException {
+    public InvalidStateOfResourceException(String message) {
+        super(message);
+    }
+}
