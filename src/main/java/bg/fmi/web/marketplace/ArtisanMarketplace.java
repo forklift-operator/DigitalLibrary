0b75e802046939c5450fe93559e95323bb13467a
Nikolay Milkov <nikolay.milkov04@gmail.com>
2026-05-10 23:09:18 +0300

    added util beans for object mapping an encryption

--- src/main/java/bg/fmi/web/marketplace/ArtisanMarketplace.java
+++ src/main/java/bg/fmi/web/marketplace/ArtisanMarketplace.java
@@ -2,15 +2,30 @@ package bg.fmi.web.marketplace;
 
 import bg.fmi.web.marketplace.model.product.Product;
 import bg.fmi.web.marketplace.repository.ProductRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
+@Configuration
 public class ArtisanMarketplace {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ArtisanMarketplace.class, args);
 	}
 
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
